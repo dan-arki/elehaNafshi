e4124ebf0b84dfbf8103cb0354ed4a2e71ae28f3
Dan <100216647+dan-arki@users.noreply.github.com>
2025-07-24 17:23:23 +0200

    Updated my-prayers.tsx

--- app/my-prayers.tsx
+++ app/my-prayers.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { ChevronLeft, Plus, Edit, Trash2, ExternalLink } from 'lucide-react-native';
+import { ChevronLeft, Plus, CreditCard as Edit, Trash2, ExternalLink } from 'lucide-react-native';
 import { Colors } from '../constants/Colors';
 import { useAuth } from '../contexts/AuthContext';
 import { getCustomPrayers, deleteCustomPrayer } from '../services/firestore';
