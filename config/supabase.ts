d1b22d533277887231010af16702c737d4670f27
Dan <100216647+dan-arki@users.noreply.github.com>
2025-10-05 23:32:21 +0200

    Updated supabase.ts

--- config/supabase.ts
+++ config/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
 
-const supabaseUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL || 'https://vjbtiehfvowxfddistkc.supabase.co';
-const supabaseAnonKey = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYnRpZWhmdm93eGZkZGlzdGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2ODQzOTQsImV4cCI6MjA3NTI2MDM5NH0.iPcVK3HGY1LnTxE4-AWUDuvogp220MLmwBiAjYF9apY';
+const supabaseUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL || process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://hiamakvbvhgzzojfzjqn.supabase.co';
+const supabaseAnonKey = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpYW1ha3ZidmhnenpvamZ6anFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2OTM2MzIsImV4cCI6MjA3NTI2OTYzMn0.JAaiq7RT5-xDVjK2NkU4RMmkXaamGmMonekkAQFujuo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
